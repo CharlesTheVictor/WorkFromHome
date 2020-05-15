06b94620d40bcd4c7797cb3fa1ced8c7f380b522
CharlesTheVictor <31681649+CharlesTheVictor@users.noreply.github.com>
2020-05-15 00:15:47 -0700

    UPDATE

---
+++ assets/js/script.js
@@ -0,0 +1,149 @@
+$('document').ready(function(){
+
+	$('.WFHlogo').click(
+		 function() {
+			 $('.grid').removeClass('scale');
+			 $('.DescAnnie, .DescAudreen, .DescBrian, .DescCharles, .DescEllie, .DescGloria, .DescHenry, .DescJaneChen, .DescJaneLu, .DescJono, .DescKei, .DescMarcus, .DescMomo, .DescMonique, .DescSteven, .DescVrinda, .DescYenne').hide();
+			 $('.module1, .module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').show();
+	}, );
+
+	$('.headerbar').click(
+		 function() {
+			 $('.grid').removeClass('scale');
+			 $('.DescAnnie, .DescAudreen, .DescBrian, .DescCharles, .DescDiya, .DescEllie, .DescGloria, .DescHenry, .DescJaneChen, .DescJaneLu, .DescJono, .DescKei, .DescMarcus, .DescMomo, .DescMonique, .DescSteven, .DescVrinda, .DescYenne').hide();
+			 $('.module1, .module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').show();
+	}, );
+
+	$('#video1').click(
+		 function() {
+       $('.grid').addClass('scale');
+       $('.DescAnnie').show();
+       $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+  $('#video2').click(
+     function() {
+       $('.grid').addClass('scale');
+       $('.DescAudreen').show();
+       $('.module1, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+  }, );
+
+  $('#video3').click(
+     function() {
+       $('.grid').addClass('scale');
+       $('.DescBrian').show();
+       $('.module2, .module1, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+  }, );
+
+	$('#video4').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescCharles').show();
+			 $('.module2, .module3, .module1, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+	$('#video5').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescDiya').show();
+			 $('.module2, .module1, .module4, .module3, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+		$('#video6').click(
+			 function() {
+				 $('.grid').addClass('scale');
+				 $('.DescEllie').show();
+				 $('.module2, .module1, .module4, .module3, .module5, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+		}, );
+
+		$('#video7').click(
+			 function() {
+				 $('.grid').addClass('scale');
+				 $('.DescGloria').show();
+				 $('.module2, .module3, .module4, .module5, .module6, .module1, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+		}, );
+
+		$('#video8').click(
+			 function() {
+				 $('.grid').addClass('scale');
+				 $('.DescHenry').show();
+				 $('.module2, .module3, .module4, .module5, .module6, .module7, .module1, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+		}, );
+
+  $('#video9').click(
+     function() {
+       $('.grid').addClass('scale');
+       $('.DescJaneChen').show();
+       $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module1, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+  }, );
+
+	$('#video10').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescJaneLu').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module1, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+	$('#video11').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescJono').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module1, .module12, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+	$('#video12').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescKei').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module1, .module13, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+	$('#video13').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescMarcus').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module1, .module14, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+	$('#video14').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescMomo').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module1, .module15, .module16, .module17, .module18').hide();
+	}, );
+
+  $('#video15').click(
+     function() {
+       $('.grid').addClass('scale');
+       $('.DescMonique').show();
+       $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module1, .module16, .module17, .module18').hide();
+  }, );
+
+	$('#video16').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescSteven').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module1, .module17, .module18').hide();
+	}, );
+
+	$('#video17').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescVrinda').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module1, .module18').hide();
+	}, );
+
+	$('#video18').click(
+		 function() {
+			 $('.grid').addClass('scale');
+			 $('.DescYenne').show();
+			 $('.module2, .module3, .module4, .module5, .module6, .module7, .module8, .module9, .module10, .module11, .module12, .module13, .module14, .module15, .module16, .module17, .module1').hide();
+	}, );
+
+	$('.loading').click(
+		 function() {
+			 $('.grid').show();
+			 $(".loading").remove();
+	}, );
+
+});
